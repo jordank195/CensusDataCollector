153efb206cc803e6de8b9320b375d62e5ec24e73
Jordan Keyton <jordan@keyton.co.uk>
2023-02-12 15:32:29 +0000

    Update to include predicates

--- JSDataCollector.mjs
+++ JSDataCollector.mjs
@@ -113,70 +113,78 @@ const geographicareas = {
     '25': 'zip-code-tabulation-area'
 }
 
-console.log(dateHex)
-//console.log("Current source path is " + sourcePathArray)
-const year = prompt("What year? ")
-var values = []
-var moreValues = 'y'
-var geographicarea = 'n'
+
+const year = prompt("What year? ")  // Prompt to enter query year
 
 
-//while(moreValues == 'y'){
-//    values.push(prompt("What value?"))
-//    moreValues = prompt("More values? y/n")
-//}
-var sourcePathEntry = prompt("Enter the source path in url format (e.g. acs/acs1): ")
-sourcePathEntry = sourcePathEntry.replace(/\s/g, '');
-sourcePathArray = sourcePathEntry.split('/')
-console.log("Current source path is " + sourcePathArray)
-var valueEntry = prompt("Enter all variables, separated by a comma (,): ")
-valueEntry = valueEntry.replace(/\s/g, '');
-values = valueEntry.split(',')
-console.log("Chosen variables are " + values)
-var workingGeo = {}
 
-//geographicarea = prompt('Geographic area? y/n')
+var sourcePathEntry = prompt("Enter the source path in url format (e.g. acs/acs1): ")   // Prompt to enter query source path
+sourcePathEntry = sourcePathEntry.replace(/\s/g, '');                                   // Tidy up source path (remove any erroneous whitespace)
+sourcePathArray = sourcePathEntry.split('/')                                            // census api requires each part of the source path as an array. Splits source into array using / as delimeter
 
-console.log(geographicarea)
 
+var values = []                                                                         // Pre-declare array used to store the variables used in the query
+var valueEntry = prompt("Enter all variables, separated by a comma (,): ")              // Prompt to enter query variables 
+valueEntry = valueEntry.replace(/\s/g, '');                                             // Tidy up variable entry to remove whitespace
+values = valueEntry.split(',')                                                          // Converts variable entry into array of variables, using commma (,) as delimeter
+
+
+
+
+
+var workingGeo = {}                                                                                     // Pre-declare new dictionary used for area-value pairs
 var areaTypes =  []
+console.log(geographicareas)                                                                            // Print out dictionary of geographical areas, assigned a number and stored as key/value pairs
+var areaTypeEntry = prompt("Enter the number for each area type, separated by a comma (,): ")           // Prompt to enter all geographical areas for query, using the number (key) that corresponds to the area (value) 
+areaTypeEntry = areaTypeEntry.replace(/\s/g, '');                                                       // Tidy up geo area entry to remove whitespace
+areaTypes = areaTypeEntry.split(',')                                                                    // Convert geo area entry into array of areas
+areaTypes.sort(function(b, a) {return parseInt(a.replace(/,/g,'') - parseInt(b.replace(/,/g,'')));});   // This sorts the array (still numbers) - the api requires parent areas first
+for(let i = 0, l = areaTypes.length; i < l; i++){                                                       // For loop will match the keys (numbers entered) with their values (the geographical area type) and then use each area type as a key for a new key/value paired dictionary
+
+    var chosenGeo = geographicareas[areaTypes[i]]                                                       // Match key to geo area
+    var areavalue = prompt("Enter area value for "+ chosenGeo + ": ")                                   // Prompt to enter value for specified geo area
+    workingGeo[chosenGeo] = areavalue                                                                   // Add area type paired with its value to new dictionary
+
+}
 
 
-//if (geographicarea == 'y'){
-    console.log(geographicareas)
-    var areaTypeEntry = prompt("Enter the number for each area type, separated by a comma (,): ")
-    areaTypeEntry = areaTypeEntry.replace(/\s/g, '');
-    areaTypes = areaTypeEntry.split(',')
-    areaTypes.sort(function(b, a) {
-    return parseInt(a.replace(/,/g,'') - parseInt(b.replace(/,/g,'')));
-    });
-    console.log(areaTypes)
-    for(let i = 0, l = areaTypes.length; i < l; i++){
+var workingPredicates = {}                                                                              // Pre-declare dictionary for predicates
+var predicateTypes = []
+var predicateCheck = prompt("Any predicates? (e.g. NAICS1997) y/n: ")                                                    // Not all queries require a predicate; checks if predicate(s) needed
+if (predicateCheck == 'y'){
+    var predicateTypeEntry = prompt("Enter each predicate type, separated by a comma (,): ")            // Prompt to enter predicate type
+    predicateTypeEntry = predicateTypeEntry.replace(/\s/g, '')                                          // Tidy up predicate type entry to remove whitespace 
+    predicateTypes = predicateTypeEntry.split(',')                                                      // Split entry into array, using comma as delimeter
+//    predicateTypes.sort(function(b, a) {
+//   return parseInt(a.replace(/,/g,'') - parseInt(b.replace(/,/g,'')));
+//   });
+//    console.log(predicateTypes)
+    for(let i = 0, l = predicateTypes.length; i < l; i++){                                               // Similar to geo entries, creates new key/value paired dictionary of predicate typres and their values
 
-        var chosenGeo = geographicareas[areaTypes[i]]
-        var areavalue = prompt("Enter area value for "+ chosenGeo + ": ")
-        workingGeo[chosenGeo] = areavalue
+        var chosenPredicate = predicateTypes[i]
+        var predicateValue = prompt("Enter predicate value for "+ chosenPredicate + ": ")
+        workingPredicates[chosenPredicate] = predicateValue
 
-    }
-    //var areavalue = prompt("Enter area value")
-    //defaultGeo = {}
-    //defaultGeo[geographicareas[areatype]] = areavalue
-    console.log(workingGeo)
+    }   
+}
 
-//}
+var consoleCountyOutput = ""    // Pre-declares values used for adding county to excel
+var consoleStateOutput = ""     // Pre-declares values used for adding state to excel
 
-var consoleCountyOutput = ""
-var consoleStateOutput = ""
 
-census(
+// TO-DO: Add comments for section below. Query is slightly different depending on if there is or isn't a predicate. Probably more efficient way to run that.
+// Also cross-references state and counties with FIPS codes
+// Also adds data to new excel sheet with current date and time in hexadecimal as sheetname.
+// Finally prints a pretty message with stars at the end
+
+if (predicateCheck == 'y'){
+    census(
     {
         vintage: year,
         geoHierarchy: workingGeo,
         sourcePath: sourcePathArray,
         values: values,
-        //predicates: {
-          //  B01001_001E: '0:100000', // number range separated by `:`
-       // },
+        predicates: workingPredicates,
         statsKey: 'c3889f5e4df523b520093ced0ab154168e094ead',
     },
     (err, res) => {
@@ -235,7 +243,7 @@ census(
         var starsMessage = '*'
         var starsSpaceMessage = '*'
 
-        for(let i = 0, l = outputMessage.length; i < l; i++) {
+        for(let i = 0, l = outputMessage.length; i < l-2; i++) {
 
             starsMessage = starsMessage + '*'
             starsSpaceMessage = starsSpaceMessage + ' '
@@ -266,5 +274,199 @@ census(
 
     }
 )
+} else{
+    census(
+    {
+        vintage: year,
+        geoHierarchy: workingGeo,
+        sourcePath: sourcePathArray,
+        values: values,
+        //predicates: workingPredicates,
+        statsKey: 'c3889f5e4df523b520093ced0ab154168e094ead',
+    },
+    (err, res) => {
+
+        var output = res
+
+        if (output != null){
+
+            //try {
+
+                for(let i = 0, l = output.length; i < l; i++) { // Converts state and county fips codes to names
+                
+                
+
+                    try{
+                        var workingState = output[i].state
+                        output[i].state = states[workingState]
+                    }
+                    catch(err){
+                        if(consoleStateOutput != "Missing state data in the output"){
+                            consoleStateOutput = "Missing state data in the output"
+                            console.log("Missing state data in the output")
+                        }
+                        
+                    }
+                    try{
+                        var workingCounty = output[i].county
+                        var fullFips = workingState + workingCounty
+                        var countyName = getCountyByFips(fullFips)
+                        output[i].county = countyName.name
+                        output[i].fips = fullFips
+                    }
+                    catch(err){
+                        
+                        if(consoleCountyOutput != "Missing county and/or state data in output"){
+                            consoleCountyOutput = "Missing county and/or state data in output"
+                            console.log("Missing county and/or state data in output")
+                        }
+                        
+                    }
+                    
+
+                }
+  
+        
+    
+
+        //var workbook = XLSX.utils.book_new();
+        var workbook = XLSX.readFile('Results.xlsb')
+        var worksheet = XLSX.utils.json_to_sheet(res);
+        XLSX.utils.book_append_sheet(workbook, worksheet, dateHex);
+        XLSX.writeFile(workbook, "Results.xlsb");
+
+        var outputMessage = '*       Results added to '+dateHex+ ' in Results.xlsb       *'
+
+        var starsMessage = '*'
+        var starsSpaceMessage = '*'
+
+        for(let i = 0, l = outputMessage.length; i < l-2; i++) {
+
+            starsMessage = starsMessage + '*'
+            starsSpaceMessage = starsSpaceMessage + ' '
+
+        }
+
+        starsMessage = starsMessage + '*'
+        starsSpaceMessage = starsSpaceMessage + '*'
+
+
+        console.log(starsMessage)
+        console.log(starsSpaceMessage)    
+        console.log(outputMessage)
+        console.log(starsSpaceMessage) 
+        console.log(starsMessage)
+
+        
+        } else{
+
+        console.log('****************')
+        console.log('*              *')    
+        console.log('*  No results  *')
+        console.log('*              *') 
+        console.log('****************')
+
+    }
+
+
+    }
+)
+}
+// census(
+//     {
+//         vintage: year,
+//         geoHierarchy: workingGeo,
+//         sourcePath: sourcePathArray,
+//         values: values,
+//         predicates: workingPredicates,
+//         statsKey: 'c3889f5e4df523b520093ced0ab154168e094ead',
+//     },
+//     (err, res) => {
+
+//         var output = res
+
+//         if (output != null){
+
+//             //try {
+
+//                 for(let i = 0, l = output.length; i < l; i++) { // Converts state and county fips codes to names
+                
+                
+
+//                     try{
+//                         var workingState = output[i].state
+//                         output[i].state = states[workingState]
+//                     }
+//                     catch(err){
+//                         if(consoleStateOutput != "Missing state data in the output"){
+//                             consoleStateOutput = "Missing state data in the output"
+//                             console.log("Missing state data in the output")
+//                         }
+                        
+//                     }
+//                     try{
+//                         var workingCounty = output[i].county
+//                         var fullFips = workingState + workingCounty
+//                         var countyName = getCountyByFips(fullFips)
+//                         output[i].county = countyName.name
+//                         output[i].fips = fullFips
+//                     }
+//                     catch(err){
+                        
+//                         if(consoleCountyOutput != "Missing county and/or state data in output"){
+//                             consoleCountyOutput = "Missing county and/or state data in output"
+//                             console.log("Missing county and/or state data in output")
+//                         }
+                        
+//                     }
+                    
+
+//                 }
+  
+        
+    
+
+//         //var workbook = XLSX.utils.book_new();
+//         var workbook = XLSX.readFile('Results.xlsb')
+//         var worksheet = XLSX.utils.json_to_sheet(res);
+//         XLSX.utils.book_append_sheet(workbook, worksheet, dateHex);
+//         XLSX.writeFile(workbook, "Results.xlsb");
+
+//         var outputMessage = '*  Results added to '+dateHex+ ' in Results.xlsb      *'
+
+//         var starsMessage = '*'
+//         var starsSpaceMessage = '*'
+
+//         for(let i = 0, l = outputMessage.length; i < l; i++) {
+
+//             starsMessage = starsMessage + '*'
+//             starsSpaceMessage = starsSpaceMessage + ' '
+
+//         }
+
+//         starsMessage = starsMessage + '*'
+//         starsSpaceMessage = starsSpaceMessage + '*'
+
+
+//         console.log(starsMessage)
+//         console.log(starsSpaceMessage)    
+//         console.log(outputMessage)
+//         console.log(starsSpaceMessage) 
+//         console.log(starsMessage)
+
+        
+//         } else{
+
+//         console.log('****************')
+//         console.log('*              *')    
+//         console.log('*  No results  *')
+//         console.log('*              *') 
+//         console.log('****************')
+
+//     }
+
+
+//     }
+// )
 
 
